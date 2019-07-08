95bba20d164b9c6dd096284f51801931b70d8441
joshgrant <joshua.m.grant@gmail.com>
2019-07-08 10:31:08 -0400

    new starting point

---
+++ tests/workflow.js
@@ -0,0 +1,27 @@
+const expect = require('chai').expect;
+
+
+describe("Login", function() {
+
+    it("workflow", function() {
+        browser.url("/")
+
+        $("#user-name").setValue("");
+        $("#password").setValue("");
+        $(".btn_action").click();	
+
+        expect($("[data-test='error']").isDisplayed()).to.be.true;
+
+        $("#user-name").setValue("invalid");
+        $("#password").setValue("invalid");
+        $(".btn_action").click();	
+
+        expect($("[data-test='error']").isDisplayed()).to.be.true;
+
+        $("#user-name").setValue("standard_user");
+        $("#password").setValue("secret_sauce");
+        $(".btn_action").click();
+
+	expect($('#header_container').isDisplayed()).to.be.true;
+    });
+});
