00483a3666c7cc7699fdf526601e04f90e45e16e
joshgrant <joshua.m.grant@gmail.com>
2020-02-25 14:07:20 -0500

    break up its

--- tests/workflow.js
+++ tests/workflow.js
@@ -1,9 +1,9 @@
 const expect = require('chai').expect;
 
 
-describe("Login", function() {
+describe("Login Tests", function() {
 
-    it("workflow", function() {
+    it("should not login with blank credentials", function() {
         browser.url("/")
 
         $("#user-name").setValue("");
@@ -11,12 +11,20 @@ describe("Login", function() {
         $(".btn_action").click();	
 
         expect($(".error-button").isDisplayed()).to.be.true;
+    });
+
+    it("should not login with invalid credentials", function() {
+        browser.url("/")
 
         $("#user-name").setValue("invalid");
         $("#password").setValue("invalid");
         $(".btn_action").click();	
 
         expect($(".error-button").isDisplayed()).to.be.true;
+    });
+
+    it("should login with valid credentials", function() {
+        browser.url("/");
 
         $("#user-name").setValue("standard_user");
         $("#password").setValue("secret_sauce");
